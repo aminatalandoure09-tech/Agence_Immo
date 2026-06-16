7578956a9068e9c1c81c2b5b2e859d0222a41a28
LandoureA9 <aminatalandoure09@gmail.com>
2026-06-16 19:49:11 +0000

    initialisation du projet

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/logements.php';
+require __DIR__.'/terrains.php';
+require __DIR__.'/clients.php';
+require __DIR__.'/agences.php';
